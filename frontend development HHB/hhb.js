00b4314ef632f76c6a53e47bae21d9a6e8ab2f44
josefienMatla <55492557+josefienMatla@users.noreply.github.com>
2019-10-16 11:44:18 +0200

    Add files via upload

---
+++ frontend development HHB/hhb.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+function myfuntion(){
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event){
+    if (!event.target.matches('.dropdown2')){
+        var dropdowns = document.getElementByClassname('dropdown-inhoud');
+
+        for (let i =0; i< dropdown.lenght; i++){
+            var openDropdown = dropdown [i];
+            if (openDropdown.classlist.contains('show')){
+                openDropdown.Classlist.remove('show');
+            }
+        }
+    }
+}
+
+
